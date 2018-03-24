afcda170eedc2567de4b3016f1330d0c544ba767
Harminder Virk <virk.officials@gmail.com>
2018-03-24 13:18:06 +0530

    chore(package): update dependencies

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,27 +1,27 @@
-import {readFileSync as read} from 'fs';
-import {join} from 'path';
-import test from 'ava';
-import remark from 'remark';
-import slug from 'remark-slug';
-import html from 'remark-html';
-import headings from '../';
+import {readFileSync as read} from 'fs'
+import {join} from 'path'
+import test from 'ava'
+import remark from 'remark'
+import slug from 'remark-slug'
+import html from 'remark-html'
+import headings from '../'
 
-let base = file => read(join(__dirname, 'fixtures', file), 'utf-8');
+let base = file => read(join(__dirname, 'fixtures', file), 'utf-8')
 
 test('should autolink headings', t => {
-    ['append', 'prepend', 'wrap'].forEach(b => {
-        let {contents} = remark().use(slug).use(html).use(headings, {behaviour: b}).processSync(base('input.md'));
-        t.deepEqual(contents, base(`output.${b}.html`), `should ${b} headings`);
-    });
-});
+  ['append', 'prepend', 'wrap'].forEach(b => {
+    let {contents} = remark().use(slug).use(html).use(headings, {behaviour: b}).processSync(base('input.md'))
+    t.deepEqual(contents, base(`output.${b}.html`), `should ${b} headings`)
+  })
+})
 
 test('should accept custom content', t => {
-    const md = '# method';
-    const {contents} = remark().use(slug).use(html).use(headings, {content: {type: 'text', value: '#'}}).processSync(md);
-    t.deepEqual(contents, '<h1 id="method"><a href="#method" aria-hidden="true">#</a>method</h1>\n');
-});
+  const md = '# method'
+  const {contents} = remark().use(slug).use(html).use(headings, {content: {type: 'text', value: '#'}}).processSync(md)
+  t.deepEqual(contents, '<h1 id="method"><a href="#method" aria-hidden="true">#</a>method</h1>\n')
+})
 
 test('should do nothing if slugs are not used', t => {
-    let {contents} = remark().use(headings).use(html).processSync(base('input.md'));
-    t.deepEqual(contents, base('output.html'));
-});
+  let {contents} = remark().use(headings).use(html).processSync(base('input.md'))
+  t.deepEqual(contents, base('output.html'))
+})
--- src/index.js
+++ src/index.js
@@ -1,72 +1,72 @@
-import visit from 'unist-util-visit';
+import visit from 'unist-util-visit'
 
-const icon = 'icon';
-const link = 'link';
-const wrap = 'wrap';
+const icon = 'icon'
+const link = 'link'
+const wrap = 'wrap'
 
 const methodMap = {
-    prepend: 'unshift',
-    append: 'push',
-};
+  prepend: 'unshift',
+  append: 'push'
+}
 
-const base = (node, callback) => {
-    const {data} = node;
-    if (!data || !data.hProperties || !data.hProperties.id) {
-        return;
-    }
+const base = (node, fn) => {
+  const {data} = node
+  if (!data || !data.hProperties || !data.hProperties.id) {
+    return
+  }
 
-    return callback('#' + data.hProperties.id);
-};
+  return fn('#' + data.hProperties.id)
+}
 
 const contentDefaults = {
-    type: 'element',
-    tagName: 'span',
-    properties: {className: [icon, `${icon}-${link}`]},
-};
+  type: 'element',
+  tagName: 'span',
+  properties: {className: [icon, `${icon}-${link}`]}
+}
 
 export default function attacher (opts = {}) {
-    let {linkProperties, behaviour, content} = {
-        behaviour: 'prepend',
-        content: contentDefaults,
-        ...opts,
-    };
+  let {linkProperties, behaviour, content} = {
+    behaviour: 'prepend',
+    content: contentDefaults,
+    ...opts
+  }
 
-    if (behaviour !== wrap && !linkProperties) {
-        linkProperties = {'aria-hidden': true};
-    }
+  if (behaviour !== wrap && !linkProperties) {
+    linkProperties = {'aria-hidden': true}
+  }
 
-    function injectNode (node) {
-        return base(node, url => {
-            if (!Array.isArray(content)) {
-                content = [content];
-            }
-            node.children[methodMap[behaviour]]({
-                type: link,
-                url,
-                title: null,
-                data: {
-                    hProperties: linkProperties,
-                    hChildren: content,
-                },
-            });
-        });
-    }
+  function injectNode (node) {
+    return base(node, url => {
+      if (!Array.isArray(content)) {
+        content = [content]
+      }
+      node.children[methodMap[behaviour]]({
+        type: link,
+        url,
+        title: null,
+        data: {
+          hProperties: linkProperties,
+          hChildren: content
+        }
+      })
+    })
+  }
 
-    function wrapNode (node) {
-        return base(node, url => {
-            const {children} = node;
+  function wrapNode (node) {
+    return base(node, url => {
+      const {children} = node
 
-            node.children = [{
-                type: link,
-                url,
-                title: null,
-                children,
-                data: {
-                    hProperties: linkProperties,
-                },
-            }];
-        });
-    }
+      node.children = [{
+        type: link,
+        url,
+        title: null,
+        children,
+        data: {
+          hProperties: linkProperties
+        }
+      }]
+    })
+  }
 
-    return ast => visit(ast, 'heading', behaviour === wrap ? wrapNode : injectNode);
+  return ast => visit(ast, 'heading', behaviour === wrap ? wrapNode : injectNode)
 }
